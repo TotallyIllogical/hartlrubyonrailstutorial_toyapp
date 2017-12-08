f5d0e499249ae063b7a085b7844e181279ff5ee1
eiaskog <emma.eia.skog@gmail.com>
2017-12-08 16:04:07 +0100

    added users model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html: 'Hello! This is the toy app from Mike Hartl Ruby on Rails tutorial'
+    render html: 'Hello! This is the Toy App from Mike Hartl Ruby on Rails Tutorial'
   end
 end
