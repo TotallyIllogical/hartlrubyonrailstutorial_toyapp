a661c8cbcef7e9f2c2f1d24b38c7c044cf34150d
eiaskog <emma.eia.skog@gmail.com>
2017-12-08 15:32:04 +0100

    added a hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def hello
+    render html: 'Hello! This is the toy app from Mike Hartl Ruby on Rails tutorial'
+  end
 end
